21cc11abd9eaee6783d6595fc4ad07b2dc8febcb
Tomasz Drozdowski <marffinn@gmail.com>
2021-12-21 22:20:07 +0100

    commit

--- renderer.js
+++ renderer.js
@@ -1,57 +1,57 @@
 let $ = require('jquery')
+
 const execute = require('child_process').exec
-const qw = require('./node_modules/quakeworld/quakeworld')
+const qw = require('./utils/quakeworld')
 const fs = require('fs')
 
-let qwServers = null
 
+fs.readFile('data/servers.json', (err, data) => {
+    if (err) throw err
+    let qwServers = JSON.parse(data)
+    let clientPing = null
+    $('.serverList').empty()
 
-let checkPing = (addre) => {
-    let svAddr = addre
-    let pingX = null
-    execute(`qstat.exe -qws ${svAddr} -old`, function (err, data) {
-        if (err) {
-            console.log(err)
-        } else {
-            pingX = data.toString()
-            console.log(pingX);
-            console.log(pingX);
-        }
-    })
-}
-checkPing("dm6.uk:27500")
-// fs.readFile('servers.json', (err, data) => {
-//     if (err) throw err
-//     qwServers = JSON.parse(data)
+    for (let i in qwServers) {
+        let serverAddress = qwServers[i].address
+        execute(`qstat.exe -qws ${serverAddress} -noconsole -json`, function (err, data) {
+            if (err) {
+                console.log(err)
+            } else {
+                qstatPing = JSON.parse(data)
+                clientPing = qstatPing[0].ping
+            }
+        })
 
-//     for (let i in qwServers) {
+        let serverIP = serverAddress.split(':')[0]
+        let serverPort = serverAddress.split(':')[1]
 
-//         let serverAddress = qwServers[i].address
-//         // let serverPing = checkPing(serverAddress)
-
-//         let serverIP = serverAddress.split(':')[0]
-//         let serverPort = serverAddress.split(':')[1]
-
-//         qw(serverIP, serverPort, 'status', [31], function (err, data) {
-//             if (err) console.log('ERROR: ', err)
-//             let oneServerPrepare =
-//                 `<tr>
-//                             <td>${data.hostname}</td>
-//                             <td>${serverPing}</td>
-//                             <td>${data.map}</td>
-//                             <td>${data.players.length}/${data.maxclients}</td>
-//                         </tr>`
-//             $('.serverList').append(oneServerPrepare)
-//             console.log(data)
-//         })
-
-
-//     }
-// });
+        qw(serverIP, serverPort, 'status', [31], function (err, data) {
+            if (err) console.log('ERROR: ', err)
+            let oneServerPrepare =
+                `<tr>
+                    <td>${data.hostname}</td>
+                    <td>${clientPing}</td>
+                    <td>${data.map}</td>
+                    <td>${data.players.length}/${data.maxclients}</td>
+                </tr>`          
+            $('.serverList').append(oneServerPrepare)
+        })
+    }
+});
 
 let refreshMasters = () => {
     execute('qstat.exe -qwm qwmaster.fodquake.net:27000 -nh -u -sort p -json -of data/servers.json', function (err, data) {
         console.log(err)
         console.log(data.toString())
     });
+}
+let checkPing = (addre) => {
+    execute(`qstat.exe -qws ${addre} -noconsole -json`, function (err, data) {
+        if (err) {
+            console.log(err)
+        } else {
+            let qsPing = JSON.parse(data)
+            clientPing = qsPing[0].ping
+        }
+    })
 }
