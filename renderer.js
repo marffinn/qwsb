bcba1eb3d85ee07917c485c15d4040ed65be0a12
Tomasz Drozdowski <marffinn@gmail.com>
2022-02-14 09:04:13 +0100

    aswd

--- renderer.js
+++ renderer.js
@@ -117,23 +117,23 @@ let checkServer = (addre) => {
         })
     }
     getInfoUpdate()
-    inRefresh = setInterval( getInfoUpdate, cycleEvery)
+    // inRefresh = setInterval( getInfoUpdate, cycleEvery)
 }
 let readServers = () => {
-    $('tbody').empty()
+    $('#properTable').empty()
     let rawdata = fs.readFileSync( path.join(__dirname, 'servers.json') )
-    let serverList = JSON.parse(rawdata);
+    let serverList = JSON.parse(rawdata)
     for (let s in serverList) {
         if( serverList[s].ping >= 80 || serverList[s].map === undefined || serverList[s].map === "?" ) continue
         else {
             let oneServerPrepare =
-                `<tr href="${serverList[s].address}" data-name="${serverList[s].name}" data-ping="${serverList[s].ping}" data-playerno="${serverList[s].numplayers}">
-                    <td class="serverName"><a href="${serverList[s].address}">${serverList[s].name}</a></td>
-                    <td class="serverPing">${serverList[s].ping}</td>
-                    <td class="serverMap">${serverList[s].map}</td>
-                    <td class="serverPlayers">${serverList[s].numplayers}/${serverList[s].maxplayers}</td>
-                </tr>`
-            $('tbody').append(oneServerPrepare)
+                `<li href="${serverList[s].address}" data-name="${serverList[s].name}" data-ping="${serverList[s].ping}" data-playerno="${serverList[s].numplayers}">
+                    <span class="serverName"><a href="${serverList[s].address}">${serverList[s].name}</a></span>
+                    <span class="serverPing">${serverList[s].ping}</span>
+                    <span class="serverMap">${serverList[s].map}</span>
+                    <span class="serverPlayers">${serverList[s].numplayers}/${serverList[s].maxplayers}</span>
+                </li>`
+            $('#properTable').append(oneServerPrepare)
         }
     }
 }
@@ -160,8 +160,8 @@ function sort_by_name() {
     let subjects = document.querySelectorAll("[data-name]")
     let subjectsArray = Array.from(subjects)
     let sorted = subjectsArray.sort(comparator_name)
-    $(".row_results").empty()
-    sorted.forEach( e => document.querySelector(".row_results").appendChild(e) )
+    $("#properTable").empty()
+    sorted.forEach( e => document.querySelector("#properTable").appendChild(e) )
 }
 function comparator_ping(a, b) {
     if (a.dataset.ping < b.dataset.ping) return -1
@@ -172,8 +172,8 @@ function sort_by_ping() {
     let subjects = document.querySelectorAll("[data-ping]")
     let subjectsArray = Array.from(subjects)
     let sorted = subjectsArray.sort(comparator_ping)
-    $(".row_results").empty()
-    sorted.forEach( e => document.querySelector(".row_results").appendChild(e) )
+    $("#properTable").empty()
+    sorted.forEach( e => document.querySelector("#properTable").appendChild(e) )
 }
 function sort_by_players() {
     readServers()
@@ -191,7 +191,7 @@ $(window).on("load resize", function () {
         'height': window.innerHeight - 103
     })
 })
-$('body').on('click', 'tbody tr', function (e) {
+$('body').on('click', '#properTable li', function (e) {
     e.preventDefault()
     clearInterval(inRefresh)
     let svAdress = $(this).attr('href')
